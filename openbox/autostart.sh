04b23b224433a5a024aea94adcdca0b2d784a520
zmx <zmx@zmx.(none)>
2013-05-24 15:18:10 +0800

    new file:   CONSOLA.TTF 	new file:   CONSOLAB.TTF 	new file:   CONSOLAI.TTF 	new file:   CONSOLAZ.TTF 	new file:   openbox/autostart.sh 	new file:   openbox/autostart.sh~ 	new file:   openbox/menu.xml 	new file:   openbox/menu.xml~ 	new file:   openbox/rc.xml 	new file:   tint2/tint2rc

---
+++ openbox/autostart.sh
@@ -0,0 +1,10 @@
+tint2 &
+(sleep 3 && conky) &
+#tilda &
+gnome-settings-daemon &
+xcompmgr -Ss -n -Cc -fF -I-10 -O-10 -D1 -t-3 -l-4 -r4 &
+feh --bg-scale ~/Pictures/black.jpg &
+urxvt &
+volwheel &
+nm-applet -sm-disable &
+empathy &
